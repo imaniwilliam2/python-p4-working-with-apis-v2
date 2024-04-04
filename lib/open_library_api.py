358a5a7a9990f54e8a61d3f5b18ad02a3de0c845
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-04-04 12:45:22 -0400

    code along complete

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -18,7 +18,7 @@ class Search:
 
         response = requests.get(URL)
         return response.content
-
+    
     def get_search_results_json(self):
         search_term = "the lord of the rings"
 
